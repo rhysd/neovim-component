f20ce3e2915f0cb214ddac34e9b151623c8d8f1b
rhysd <lin90162@yahoo.co.jp>
2015-11-25 01:48:09 +0900

    fix specifying font size
    actual size and specified size are confusing :(

--- src/neovim/screen.ts
+++ src/neovim/screen.ts
@@ -2,26 +2,30 @@ import Store, {Cursor, FontAttributes} from './store';
 import Dispatcher from './dispatcher';
 import {updateFontSize} from './actions';
 
-interface Font {
-    width: number;
-    height: number;
-}
-
 export default class NeovimScreen {
     ctx: CanvasRenderingContext2D;
     lines: number;
     columns: number;
-    font: Font;
 
-    constructor(public canvas: HTMLCanvasElement, font_size: number) {
-        this.font = {
-            width: font_size / 2,
-            height: font_size,
-        };
+    constructor(public canvas: HTMLCanvasElement, public specifed_font_px: number) {
         this.ctx = this.canvas.getContext('2d');
 
         Store.on('put', this.drawText.bind(this));
         Store.on('clear-all', this.clearAll.bind(this));
+    }
+
+    initializeCanvas() {
+        const w = this.canvas.clientWidth;
+        const h = this.canvas.clientHeight;
+
+        this.ctx.font = this.specifed_font_px + 'px monospace'; // TODO: Enable to specify font
+        const font_width = this.ctx.measureText('m').width;
+        const font_height = font_width * 2;
+
+        Dispatcher.dispatch(updateFontSize(font_width, font_height));
+
+        this.lines = Math.floor(h / font_height);
+        this.columns = Math.floor(w / font_width);
     }
 
     clearAll() {
@@ -35,20 +39,6 @@ export default class NeovimScreen {
         this.drawBlock(line, col, 1, clear_length, Store.bg_color);
     }
 
-    initializeCanvas() {
-        const w = this.canvas.clientWidth;
-        const h = this.canvas.clientHeight;
-
-        this.ctx.font = this.font.height + 'px monospace'; // TODO: Enable to specify font
-        const font_width = this.ctx.measureText('m').width;
-        const font_height = font_width * 2;
-
-        Dispatcher.dispatch(updateFontSize(font_width, font_height));
-
-        this.lines = Math.floor(h / font_height);
-        this.columns = Math.floor(w / font_width);
-    }
-
     drawText(chars: string[][]) {
         const {line, col} = Store.cursor;
         const {fg, bg, width, height} = Store.font_attr;
@@ -58,7 +48,7 @@ export default class NeovimScreen {
 
         // TODO: Enable to specify font
         // TODO: Consider font attributes (e.g. underline, bold, ...)
-        this.ctx.font = height + 'px monospace';
+        this.ctx.font = this.specifed_font_px + 'px monospace';
         this.ctx.textBaseline = 'top';
         this.ctx.fillStyle = fg;
         const text = chars.map(c => (c[0] || '')).join('');
