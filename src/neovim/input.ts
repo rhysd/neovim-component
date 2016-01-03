008ffbdaa1f0154d7de03398b04da849a0e1bfd7
rhysd <lin90162@yahoo.co.jp>
2016-01-03 14:08:54 +0900

    add missing special keys
    https://github.com/rhysd/NyaoVim/issues/14

--- src/neovim/input.ts
+++ src/neovim/input.ts
@@ -21,6 +21,8 @@ export default class NeovimInput {
             case 9:   return 'Tab';
             case 10:  return 'NL';
             case 13:  return 'CR';
+            case 33:  return 'PageUp';
+            case 34:  return 'PageDown';
             case 27:  return 'Esc';
             case 32:  return 'Space';
             case 35:  return 'End';
@@ -29,10 +31,24 @@ export default class NeovimInput {
             case 38:  return 'Up';
             case 39:  return 'Right';
             case 40:  return 'Down';
+            case 45:  return 'Insert';
             case 46:  return 'Del';
+            case 47:  return 'Help';
             case 92:  return 'Bslash';
-            case 124: return 'Bar';
-            case 127: return 'Del';
+            case 112: return 'F1';
+            case 113: return 'F2';
+            case 114: return 'F3';
+            case 115: return 'F4';
+            case 116: return 'F5';
+            case 117: return 'F6';
+            case 118: return 'F7';
+            case 119: return 'F8';
+            case 120: return 'F9';
+            case 121: return 'F10';
+            case 122: return 'F11';
+            case 123: return 'F12';
+            case 124: return 'Bar'; // XXX
+            case 127: return 'Del'; // XXX
             case 188: return shift ? 'LT' : null;
             default:  return null;
         }
