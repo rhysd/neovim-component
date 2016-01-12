848a53973768b69abb839f85b7c5cad675c37087
rhysd <lin90162@yahoo.co.jp>
2016-01-12 19:49:09 +0900

    add tests for 'input' event on <input>

--- test/unit/input_test.js
+++ test/unit/input_test.js
@@ -4,28 +4,47 @@ const jsdom = require('jsdom').jsdom;
 const NeovimStore = require('../../src/out/neovim/store').default;
 const NeovimInput = require('../../src/out/neovim/input').default;
 
-function keydownEvent(char, opts) {
+function keydownEvent(opts) {
     const o = opts || {};
     if (o.keyCode) {
         o.charCode = o.which = o.keyCode;
         o.key = String.fromCharCode(o.keyCode);
+    } else if (o.key) {
+        o.keyCode = o.charCode = o.which = o.key.charCodeAt(0);
     } else {
-        o.key = char;
-        o.keyCode = o.charCode = o.which = char.charCodeAt(0);
+        throw 'Invalid options for KeyboardEvent';
     }
     return new window.KeyboardEvent('keydown', o);
 }
 
-function dispatchKeydown(char, opts) {
-    return global.input_element.dispatchEvent(keydownEvent(char, opts));
+function dispatchKeydown(opts) {
+    return global.input_element.dispatchEvent(keydownEvent(opts));
 }
 
-function inputByKeydown(char, opts) {
+function inputByKeydown(opts) {
     global.last_input = '';
-    dispatchKeydown(char, opts);
+    dispatchKeydown(opts);
     const i = global.last_input;
     global.last_input = '';
     return i;
+}
+
+function dispatchInputEvent() {
+    global.input_element.dispatchEvent(
+        new window.Event('input', {
+            bubbles: true,
+            cancelable: false
+        })
+    );
+}
+
+function catchInputOnInputEvent(i) {
+    global.last_input = '';
+    global.input_element.value = i;
+    dispatchInputEvent();
+    const tmp = global.last_input;
+    global.last_input = '';
+    return tmp;
 }
 
 describe('NeovimInput', () => {
@@ -59,54 +78,67 @@ describe('NeovimInput', () => {
         it('ignores normal char input without modifiers', () => {
             global.last_input = '';
 
-            dispatchKeydown('a');
+            dispatchKeydown({key: 'a'});
             assert.equal(last_input, '');
 
-            dispatchKeydown('[');
+            dispatchKeydown({key: '['});
             assert.equal(last_input, '');
 
-            dispatchKeydown('3', {
-                shiftKey: true
-            });
+            dispatchKeydown({key: '3', shiftKey: true});
             assert.equal(last_input, '');
         });
 
         it('accepts input with ctrl key', () => {
-            assert.equal(inputByKeydown('a', {ctrlKey: true}), '<C-a>');
-            assert.equal(inputByKeydown('3', {ctrlKey: true}), '<C-3>');
-            assert.equal(inputByKeydown('[', {ctrlKey: true}), '<C-[>');
-            assert.equal(inputByKeydown('[', {ctrlKey: true, shiftKey: true}), '<C-S-[>');
+            assert.equal(inputByKeydown({key: 'a', ctrlKey: true}), '<C-a>');
+            assert.equal(inputByKeydown({key: '3', ctrlKey: true}), '<C-3>');
+            assert.equal(inputByKeydown({key: '[', ctrlKey: true}), '<C-[>');
+            assert.equal(inputByKeydown({key: '[', ctrlKey: true, shiftKey: true}), '<C-S-[>');
         });
 
         it('accepts input with alt key', () => {
-            assert.equal(inputByKeydown(',', {altKey: true}), '<A-,>');
+            assert.equal(inputByKeydown({key: ',', altKey: true}), '<A-,>');
             // assert.equal(inputByKeydown('r', {altKey: true}), '<A-r>');
-            assert.equal(inputByKeydown('4', {altKey: true}), '<A-4>');
+            assert.equal(inputByKeydown({key: '4', altKey: true}), '<A-4>');
             // assert.equal(inputByKeydown('.', {altKey: true, shiftKey: true}), '<A-S-.>');
         });
 
         it('accepts input with alt and ctrl key', () => {
-            assert.equal(inputByKeydown(',', {altKey: true, ctrlKey: true}), '<C-A-,>');
-            assert.equal(inputByKeydown('a', {altKey: true, ctrlKey: true}), '<C-A-a>');
-            assert.equal(inputByKeydown('a', {altKey: true, ctrlKey: true, shiftKey: true}), '<C-A-S-a>');
+            assert.equal(inputByKeydown({key: ',', altKey: true, ctrlKey: true}), '<C-A-,>');
+            assert.equal(inputByKeydown({key: 'a', altKey: true, ctrlKey: true}), '<C-A-a>');
+            assert.equal(inputByKeydown({key: 'a', altKey: true, ctrlKey: true, shiftKey: true}), '<C-A-S-a>');
         });
 
         it('accepts special keys', () => {
-            assert.equal(inputByKeydown('', {keyCode: 9}), '<Tab>');
-            assert.equal(inputByKeydown('', {keyCode: 9, altKey: true}), '<A-Tab>');
-            assert.equal(inputByKeydown('', {keyCode: 9, ctrlKey: true, shiftKey: true}), '<C-S-Tab>');
-            assert.equal(inputByKeydown('', {keyCode: 13}), '<CR>');
-            assert.equal(inputByKeydown('', {keyCode: 13, altKey: true}), '<A-CR>');
-            assert.equal(inputByKeydown('', {keyCode: 13, ctrlKey: true, shiftKey: true}), '<C-S-CR>');
-            assert.equal(inputByKeydown('', {keyCode: 37}), '<Left>');
-            assert.equal(inputByKeydown('', {keyCode: 37, altKey: true}), '<A-Left>');
-            assert.equal(inputByKeydown('', {keyCode: 37, ctrlKey: true, shiftKey: true}), '<C-S-Left>');
-            assert.equal(inputByKeydown('', {keyCode: 92}), '<Bslash>');
-            assert.equal(inputByKeydown('', {keyCode: 92, altKey: true}), '<A-Bslash>');
-            assert.equal(inputByKeydown('', {keyCode: 92, ctrlKey: true, shiftKey: true}), '<C-S-Bslash>');
-            assert.equal(inputByKeydown('', {keyCode: 188, shiftKey: true}), '<LT>');
-            assert.equal(inputByKeydown('', {keyCode: 188, ctrlKey: true, shiftKey: true}), '<C-LT>');
-            assert.equal(inputByKeydown('', {keyCode: 188}), '');
+            assert.equal(inputByKeydown({keyCode: 9}), '<Tab>');
+            assert.equal(inputByKeydown({keyCode: 9, altKey: true}), '<A-Tab>');
+            assert.equal(inputByKeydown({keyCode: 9, ctrlKey: true, shiftKey: true}), '<C-S-Tab>');
+            assert.equal(inputByKeydown({keyCode: 13}), '<CR>');
+            assert.equal(inputByKeydown({keyCode: 13, altKey: true}), '<A-CR>');
+            assert.equal(inputByKeydown({keyCode: 13, ctrlKey: true, shiftKey: true}), '<C-S-CR>');
+            assert.equal(inputByKeydown({keyCode: 37}), '<Left>');
+            assert.equal(inputByKeydown({keyCode: 37, altKey: true}), '<A-Left>');
+            assert.equal(inputByKeydown({keyCode: 37, ctrlKey: true, shiftKey: true}), '<C-S-Left>');
+            assert.equal(inputByKeydown({keyCode: 92}), '<Bslash>');
+            assert.equal(inputByKeydown({keyCode: 92, altKey: true}), '<A-Bslash>');
+            assert.equal(inputByKeydown({keyCode: 92, ctrlKey: true, shiftKey: true}), '<C-S-Bslash>');
+            assert.equal(inputByKeydown({keyCode: 188, shiftKey: true}), '<LT>');
+            assert.equal(inputByKeydown({keyCode: 188, ctrlKey: true, shiftKey: true}), '<C-LT>');
+            assert.equal(inputByKeydown({keyCode: 188}), '');
+        });
+    });
+
+    context("on 'input' event", () => {
+        it('sends input character to Neovim', () => {
+            assert.equal(catchInputOnInputEvent('a'), 'a');
+            assert.equal(catchInputOnInputEvent('3'), '3');
+            assert.equal(catchInputOnInputEvent(';'), ';');
+            assert.equal(catchInputOnInputEvent('^'), '^');
+        });
+
+        it('clears value in <input>', () => {
+            global.input_element.value = 'a';
+            dispatchInputEvent();
+            assert.equal(global.input_element.value, '');
         });
     });
 });
