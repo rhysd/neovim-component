2e4e1b518aa4f9dbb55e59b31afb46e81d0ea449
rhysd <lin90162@yahoo.co.jp>
2016-03-04 11:26:52 +0900

    add delay to inhibit cursor flickering

--- src/neovim/cursor.ts
+++ src/neovim/cursor.ts
@@ -4,19 +4,23 @@ import {dragEnd} from './actions';
 
 function invertColor(image: ImageData) {
     'use strict';
-    for (let i = 0; i < image.data.length; i += 4) {
+    for (let i = 0; i < image.data.length; i+=4) {
         image.data[i] = 255 - image.data[i];     // Red
-        image.data[i + 1] = 255 - image.data[i + 1]; // Green
-        image.data[i + 2] = 255 - image.data[i + 2]; // Blue
+        image.data[i+1] = 255 - image.data[i+1]; // Green
+        image.data[i+2] = 255 - image.data[i+2]; // Blue
     }
     return image;
 }
 
+const DRAW_DELAY = 20; // millisec
+
 export default class NeovimCursor {
     private element: HTMLCanvasElement;
     private ctx: CanvasRenderingContext2D;
+    private timer_id: NodeJS.Timer;
 
     constructor(private store: NeovimStore, private screen_ctx: CanvasRenderingContext2D) {
+        this.timer_id = null;
         this.element = document.querySelector('.neovim-cursor') as HTMLCanvasElement;
         this.element.style.top = '0px';
         this.element.style.left = '0px';
@@ -42,13 +46,20 @@ export default class NeovimCursor {
     }
 
     redraw() {
-        this.ctx.clearRect(0, 0, this.element.width, this.element.height);
-        const cursor_width = this.store.mode === 'insert' ? (window.devicePixelRatio || 1) : this.store.font_attr.draw_width;
-        const cursor_height = this.store.font_attr.draw_height;
-        const x = this.store.cursor.col * this.store.font_attr.draw_width;
-        const y = this.store.cursor.line * this.store.font_attr.draw_height;
-        const captured = this.screen_ctx.getImageData(x, y, cursor_width, cursor_height);
-        this.ctx.putImageData(invertColor(captured), 0, 0);
+        if (this.timer_id !== null) {
+            clearTimeout(this.timer_id);
+        } else {
+            this.ctx.clearRect(0, 0, this.element.width, this.element.height);
+        }
+        this.timer_id = setTimeout(() => {
+            this.timer_id = null;
+            const cursor_width = this.store.mode === 'insert' ? (window.devicePixelRatio || 1) : this.store.font_attr.draw_width;
+            const cursor_height = this.store.font_attr.draw_height;
+            const x = this.store.cursor.col * this.store.font_attr.draw_width;
+            const y = this.store.cursor.line * this.store.font_attr.draw_height;
+            const captured = this.screen_ctx.getImageData(x, y, cursor_width, cursor_height);
+            this.ctx.putImageData(invertColor(captured), 0, 0);
+        }, DRAW_DELAY);
     }
 
     updateCursorPos() {
