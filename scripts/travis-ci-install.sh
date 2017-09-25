66bf2880f50d54bcd21dae06d78ced9e7a8f2813
rhysd <lin90162@yahoo.co.jp>
2017-09-25 20:36:52 +0900

    avoid an error for packages already installed

--- scripts/travis-ci-install.sh
+++ scripts/travis-ci-install.sh
@@ -10,9 +10,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     export DISPLAY=':99.0'
     sh -e /etc/init.d/xvfb start
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew install neovim pkg-config cairo pango libpng giflib
-    # jpeg is already installed
-    brew upgrade jpeg
+    brew install neovim pkg-config cairo pango libpng giflib jpeg || true
 else
     echo "Unknown platform: ${TRAVIS_OS_NAME}"
     exit 1
