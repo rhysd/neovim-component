4e9e8309f75d6a5bbcacb30247ae769c4343b30f
rhysd <lin90162@yahoo.co.jp>
2017-10-31 18:09:22 +0900

    fix duplicate in scripts for Travis CI

--- scripts/travis-ci-install.sh
+++ scripts/travis-ci-install.sh
@@ -25,18 +25,3 @@ nvim --version
 ./node_modules/.bin/browserify --version
 ./node_modules/.bin/mocha --version
 ./node_modules/.bin/electron --version
-
-echo 'Building package'
-npm run build
-npm run lint
-
-echo 'Running unit tests'
-npm run test
-
-echo 'Running E2E tests'
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    # E2E tests fail on Linux worker because of Hardware Acceleration.
-    # It may work with the environment HA is disabled. But it does not make sense to run tests
-    # without HA because most users enable HA usually.
-    npm run e2e
-fi
