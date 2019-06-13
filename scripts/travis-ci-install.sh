5753718b21fd36d93bb8c2448b24552e625493ff
rhysd <lin90162@yahoo.co.jp>
2019-06-13 12:09:00 +0900

    travis: use homebrew addon instead of using it directly

--- scripts/travis-ci-install.sh
+++ scripts/travis-ci-install.sh
@@ -9,9 +9,6 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo apt-get install -y neovim libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
     export DISPLAY=':99.0'
     sh -e /etc/init.d/xvfb start
-elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew update
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install neovim pkg-config cairo pango libpng giflib jpeg
 else
     echo "Unknown platform: ${TRAVIS_OS_NAME}"
     exit 1
